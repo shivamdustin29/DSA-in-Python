886801e94942580dc304bb2e2c5a6d12e88e714b
shivamdustin29 <75965561+shivamdustin29@users.noreply.github.com>
2022-04-05 01:11:46 +0530

    Create BalancedParenthesis.py

---
+++ BalancedParenthesis.py
@@ -0,0 +1,41 @@
+def isMatching(openingBracket, closingBracket):
+    if(openingBracket is '{' and closingBracket is '}'):
+        return True
+    if(openingBracket is '(' and closingBracket is ')'):
+        return True
+    if(openingBracket is '[' and closingBracket is ']'):
+        return True
+    return False
+
+
+
+def isBalanced(expression):
+
+    
+    stack = []
+        
+    
+    for i in range(len(expression)):
+       
+        if(expression[i] is '{' or expression[i] is '(' or expression[i] is '['):
+            stack.append(expression[i])
+        else:
+            
+            if(len(stack) == 0):
+                return False
+
+           
+            if(isMatching(stack.pop(), expression[i]) is not True):
+                return False
+
+       
+    if(len(stack) == 0):
+        return True
+    return False
+
+if __name__ == '__main__':
+
+    expression1 = "[[()]]"
+    expression2 = "[[))"
+    print(isBalanced(expression1))
+    print(isBalanced(expression2))
