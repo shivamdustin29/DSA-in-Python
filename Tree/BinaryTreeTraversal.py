d7e81480a81f3c84190fedee6ed7f0a1d3436015
shivamdustin29 <75965561+shivamdustin29@users.noreply.github.com>
2022-04-20 21:24:32 +0530

    Create BinaryTreeTraversal.py

---
+++ Tree/BinaryTreeTraversal.py
@@ -0,0 +1,63 @@
+
+# Output:
+# Pre Order Traversal is: 1 2 4 5 3 6 7 
+# In Order Traversal is: 4 2 5 1 6 3 7 
+# Post Order Traversal is: 4 5 2 6 7 3 1 
+
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.right = self.left = None
+
+
+# Defining class for the Binary Tree
+class BinaryTreeTraversal:
+    
+  
+    def __init__(self): 
+        self.root = None
+    
+    # Pre Order Traversal
+    def preOrder(self, root):
+        if(root is None):
+            return
+        print(root.data, end = " ")
+        self.preOrder(root.left)
+        self.preOrder(root.right)
+
+
+    # In Order Traversal
+    def inOrder(self, root):
+        if(root is None):
+            return
+        self.inOrder(root.left)
+        print(root.data, end = " ")
+        self.inOrder(root.right)
+
+    # Post Order Traversal */
+    def postOrder(self, root):
+        if(root is None):
+            return
+        self.postOrder(root.left)
+        self.postOrder(root.right)
+        print(root.data, end = " ")
+    
+#main method
+if __name__ == '__main__':
+    tree = BinaryTreeTraversal()
+    tree.root = Node(1)
+    tree.root.left = Node(2)
+    tree.root.right = Node(3)
+    tree.root.left.left = Node(4)
+    tree.root.left.right = Node(5)
+    tree.root.right.left = Node(6)
+    tree.root.right.right = Node(7)
+    print("Pre Order Traversal is:",  end = " ")
+    tree.preOrder(tree.root)
+    print()
+    print("In Order Traversal is:", end = " ")
+    tree.inOrder(tree.root)
+    print()
+    print("Post Order Traversal is:", end = " ")
+    tree.postOrder(tree.root)
+    print()
