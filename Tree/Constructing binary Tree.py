f110b8b3a78667df73a930125475ae9cf071328b
shivamdustin29 <75965561+shivamdustin29@users.noreply.github.com>
2022-04-20 21:20:35 +0530

    Create Constructing binary Tree.py

---
+++ Tree/Constructing binary Tree.py
@@ -0,0 +1,23 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.right = self.left = None
+
+
+# Defining class for the Binary Tree
+class BinaryTree:
+    
+    
+    def __init__(self): 
+        self.root = None
+
+# main method
+if __name__ == '__main__':
+    tree = BinaryTree()
+    tree.root = Node(1)
+    tree.root.left = Node(2)
+    tree.root.right = Node(3)
+    tree.root.left.left = Node(4)
+    tree.root.left.right = Node(5)
+    tree.root.right.left = Node(6)
+    tree.root.right.right = Node(7)
